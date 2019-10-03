0f8615d99c2d6d7bcbd488608f75714c5f927d9d
Arshia <youthinkasido@gmail.com>
2019-10-03 17:36:13 -0400

    Fix bug with window size

--- email.js
+++ email.js
@@ -1,8 +1,8 @@
 
 const Swal = require('sweetalert2')
 function runScript() {
-    let contactTitle = document.getElementById('contact')
-    contactTitle.innerHTML = 'it worked!!'
+    // let contactTitle = document.getElementById('contact')
+    // contactTitle.innerHTML = 'it worked!!'
     // Swal.fire({
     //     title: "<i>Title</i>",
     //     html: "Testno  sporocilo za objekt: <b>test</b>",
